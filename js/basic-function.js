1199c3f2f59382814142fece979eaa2aa06522c2
dinhngocbui <buidinhbaongoc@gmail.com>
2021-04-15 00:35:49 +0100

    5

--- js/basic-function.js
+++ js/basic-function.js
@@ -1,1 +1,9 @@
+// Create a variable called msg to hold a new message
 var msg = "Hello this is some testing message";
+
+// Create a function to update the content of an element(id=msg)
+function updateMessage(){
+    doucument.getElementById('message').textContent = msg;
+}
+
+updateMessage();
